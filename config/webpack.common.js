89c07409055440ae95d7bd8a567dc8b5186483a6
Nick Roberts <djparadyme@gmail.com>
2016-05-16 17:43:48 -0400

    Iniital commit

---
+++ config/webpack.common.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/client/polyfills.ts',
+    'vendor': './src/client/vendor.ts',
+    'app': './src/client/main.ts'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/client/index.html'
+    })
+  ]
+};
---
+++ src/client/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../sass/app.scss';
+@Component({
+  selector: 'my-app',
+  template: require('./app.component.html'),
+  styleUrls: [require('./app.component.scss')]
+})
+export class AppComponent { }
