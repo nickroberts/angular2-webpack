8df5785143fd4ca74ec9a5875026a617d0cf0d0d
Nick Roberts <djparadyme@gmail.com>
2016-05-17 17:32:41 -0400

    Add codelyzer

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import '../sass/app.scss';
 
 @Component({
-  selector: 'my-app',
+  selector: 'a2wp-app',
   template: require('./app.component.html'),
   styles: [ require('./app.component.scss') ]
 })
